9e16dfa446015f4a05fbc7f5183798e66c186b57
Jim Girardeau <jwgirardeau@gmail.com>
2019-01-03 21:11:15 -0600

    almost working part 3

---
+++ bamazonSupervisor.js
@@ -0,0 +1,81 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+
+connection.connect(function(err) {
+    if (err) throw err;
+    printDatabase();
+});
+
+function startPrompts() {
+    inquirer.prompt({
+        type: "list",
+        name: "selection",
+        message: "Select an action",
+        choices: ["View Product Sales by Department", "Create New Department"]
+    }).then(function(answer) {
+        if (answer.selection === "View Product Sales by Department") {
+            process.stdout.write('\033c');
+            return printDatabase();
+        } else if (answer.selection === "Create New Department") {
+            return addDepartment();
+        } else return printDatabase();
+    });
+}
+
+function printDatabase() {
+    connection.query("SELECT * FROM departments ", function(err, res) {
+        if (err) throw err;
+        //  console.log(res);
+        // res.forEach(element => {
+        //     console.log("id: " + element.item_id +
+        //         ", name: " + element.product_name +
+        //         ", dept: " + element.department_name +
+        //         ", price: $" + Number(element.price).toFixed(2) +
+        //         ", qty: " + element.stock_quantity
+        //     );
+        // });
+        console.table(res);
+        startPrompts();
+    });
+}
+
+
+function addDepartment() {
+    inquirer
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "Department: "
+    }, {
+        name: "overhead",
+        type: "input",
+        message: "Over Head: "
+    }]).then(function(answer) {
+        console.log("You want to add to " + answer.id);
+        if (answer.id.length === 0) return printDatabase();
+        if (answer.overhead.length === 0) return printDatabase();
+        if (isNaN(answer.overhead)) return printDatabase();
+        var sql = `INSERT INTO departments (department_name, over_head_costs) VALUES ("${answer.id}",${answer.overhead})`;
+        console.log(sql)
+        connection.query(sql, function(err, result) {
+            if (err) throw err;
+            return printDatabase();
+        });
+    });
+}
